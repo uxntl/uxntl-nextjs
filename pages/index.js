5ba1ba56d4154f19bf28e3066427904f006c07b8
UXNTL <uxntlco@gmail.com>
2021-01-19 17:58:03 +0700

    Second

--- pages/index.js
+++ pages/index.js
@@ -15,24 +15,30 @@ export default function Home() {
   <p>UXNTL.co</p>
   <p className="text-sm text-white font-normal italic">Graphic Design</p>
 </div>
-<div className="mt-5 w-80 flex flex-col gap-4 text-white text-base font-semibold">
-<a styles="display:block" href="http://justinbieber.com">
-<div className="transition duration-500 ease-in-out hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 flex py-4 justify-center border-2 border-red-700">Website</div>
+<div className="mt-5 flex flex-col space-y-4 w-80 text-white text-base font-semibold">
+<a styles="display:block" target="_blank" href="">
+<div className="transition duration-500 ease-in-out hover:bg-red-800 transform hover:-translate-y-1 hover:scale-110 flex justify-center border-2 border-red-700 p-4">Website</div>
 </a>
-<a styles="display:block" href="http://justinbieber.com">
-<div className="transition duration-500 ease-in-out hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 flex py-4 justify-center border-2 border-red-700">Shop</div>
+<a styles="display:block" target="_blank" href="">
+<div className="transition duration-500 ease-in-out hover:bg-red-800 transform hover:-translate-y-1 hover:scale-110 flex justify-center border-2 border-red-700 p-4"><p className="line-through">Shop</p></div>
 </a>
-<a styles="display:block" href="http://justinbieber.com">
-<div className="transition duration-500 ease-in-out hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 flex py-4 justify-center border-2 border-red-700">Whatsapp</div>
+<a styles="display:block" target="_blank" href="https://www.instagram.com/unxtl.co/?hl=en">
+<div className="transition duration-500 ease-in-out hover:bg-red-800 transform hover:-translate-y-1 hover:scale-110 flex justify-center border-2 border-red-700 p-4">Instagram</div>
 </a>
-<a styles="display:block" href="http://justinbieber.com">
-<div className="transition duration-500 ease-in-out hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 flex py-4 justify-center border-2 border-red-700">Telegram</div>
+<a styles="display:block" target="_blank" href="http://justinbieber.com">
+<div className="transition duration-500 ease-in-out hover:bg-red-800 transform hover:-translate-y-1 hover:scale-110 flex justify-center border-2 border-red-700 p-4">Telegram</div>
 </a>
-<a styles="display:block" href="http://justinbieber.com">
-<div className="transition duration-500 ease-in-out hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 flex py-4 justify-center border-2 border-red-700">Portofolio</div>
+<a styles="display:block" target="_blank" href="http://justinbieber.com">
+<div className="transition duration-500 ease-in-out hover:bg-red-800 transform hover:-translate-y-1 hover:scale-110 flex justify-center border-2 border-red-700 p-4">Portofolio</div>
 </a>
 
     </div>
+<div className="flex items-center mt-24 lg:mt-80">
+<div className="flex items-center text-sm text-gray-400 font-normal italic">
+  <p>Made with</p> <img className="h-5 w-5 mx-1 animate-pulse bg-black opacity-75" src="like.png" alt=""/> by <a className="mx-1 transition duration-500 ease-in-out bg-black hover:text-red-700  transform hover:-translate-y-1 hover:scale-110" target="_blank" href="https://github.com/uxntl">UXNTL.co</a>
+</div>
+
+</div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
   theme: {
     extend: {},
   },
+  screens: {
+    'sm': {'min': '640px', 'max': '767px'},
+    'md': {'min': '768px', 'max': '1023px'},
+    'lg': {'min': '1024px', 'max': '1279px'},
+    'xl': {'min': '1280px', 'max': '1535px'},
+    '2xl': {'min': '1536px'},
+  },
   variants: {
     extend: {},
   },
   plugins: [],
+
 }
